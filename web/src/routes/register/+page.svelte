6bb372fbc0023d165e20510b73ac7e784889fe68
Kyle Horton <kylehorton33@gmail.com>
2023-11-30 17:34:40 -0500

    fix: :label: fix typo

--- web/src/routes/register/+page.svelte
+++ web/src/routes/register/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex flex-col mt-8">
     <form action="?/register" method="POST" class="space-y-4">
-        <h3 class="text-2xl text-center">Login:</h3>
+        <h3 class="text-2xl text-center">Register:</h3>
         <Input type="email" name="email" label="E-mail" />
         <Input type="password" name="password" label="Password" />
         <Input
