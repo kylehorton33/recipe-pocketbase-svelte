7bd5b6ca3803e697099394a0a650b0a4e62255d2
Kyle Horton <kylehorton33@gmail.com>
2023-11-30 16:45:09 -0500

    fix: :pencil2: typo in form action

--- web/src/routes/register/+page.svelte
+++ web/src/routes/register/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex flex-col mt-8">
-    <form action="?/login" method="POST" class="space-y-4">
+    <form action="?/register" method="POST" class="space-y-4">
         <h3 class="text-2xl text-center">Login:</h3>
         <Input type="email" name="email" label="E-mail" />
         <Input type="password" name="password" label="Password" />
