6667fd57aea9b82958aaad26626cbd02e1ca29ef
Kyle Horton <kylehorton33@gmail.com>
2023-12-04 18:58:47 -0500

    :rocket: deploy v0.4.0 - search bar implementation

--- README.md
+++ README.md
@@ -18,9 +18,10 @@
 
 ## Roadmap :chart_with_upwards_trend:
 
-- :white_check_mark: v1: register/login/logout features
-- :white_check_mark: v2: 'recipes' collection, CRUD operations
-- :white_check_mark: v3: expand recipe features
-- :white_check_mark: v3.1: update all fields in recipe, fix nav, responsive design
-- :white_check_mark: v3.2: add `color` to recipes, UI tweaking, start to implement testing
+- :white_check_mark: v0.1.0: register/login/logout features
+- :white_check_mark: v0.2.0: 'recipes' collection, CRUD operations
+- :white_check_mark: v0.3.0: expand recipe features
+- :white_check_mark: v0.3.1: update all fields in recipe, fix nav, responsive design
+- :white_check_mark: v0.3.2: add `color` to recipes, UI tweaking, start to implement testing
+- :white_check_mark: v0.4.0: implement search bar
 
--- web/src/routes/recipes/+page.svelte
+++ web/src/routes/recipes/+page.svelte
@@ -9,8 +9,10 @@
     $: isSearchVisible = false;
 
     $: filteredRecipes = data.recipes.filter((recipe) => {
-        return recipe.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-            recipe.ingredients.toLowerCase().includes(searchTerm.toLowerCase());
+        return (
+            recipe.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            recipe.ingredients.toLowerCase().includes(searchTerm.toLowerCase())
+        );
     });
 </script>
 
@@ -24,7 +26,7 @@
             aria-hidden="true"
             on:click|stopPropagation
             transition:fly={{ duration: 300, y: 50 }}
-            class="hide-scrollbar relative mx-5 h-[400px] w-[500px] overflow-y-scroll rounded-md bg-white/20 outline outline-2 outline-white/40"
+            class="relative mx-5 w-[500px] rounded-md bg-white/20 outline outline-2 outline-white/40"
         >
             <div
                 class="sticky inset-x-0 top-0 flex w-full items-center justify-between gap-2 border-b-[1.5px] border-b-white/40 bg-black/30 py-2 pl-3 pr-2"
@@ -39,30 +41,36 @@
                     placeholder="Search recipes"
                 />
             </div>
-            <div class="w-full p-3">
-                <p class="my-1 text-white/50">
-                    {#if filteredRecipes.length === 0}
-                        No recipes found.
-                    {:else}
-                        Showing <span class="text-main"
-                            >{filteredRecipes.length}</span
-                        >
-                        {filteredRecipes.length === 1 ? "recipe" : "recipes"}
-                    {/if}
-                </p>
-
-                {#each filteredRecipes as { name, slug, ingredients }}
-                    <a
-                        href="/recipes/{slug}"
-                        class="flex w-full items-center justify-between rounded-md bg-transparent p-3 transition hover:bg-white/5"
-                    >
-                        <div>
-                            <h3 class="text-start">{name}: <small>{ingredients}</small></h3>
-                            
-                        </div>
-                    </a>
-                {/each}
-            </div>
+            {#if searchTerm}
+                <div class="w-full p-3" transition:fly={{ duration: 150, y: 50 }}>
+                    <p class="my-1 text-white/50">
+                        {#if filteredRecipes.length === 0}
+                            No recipes found.
+                        {:else}
+                            Found <span class="text-main"
+                                >{filteredRecipes.length}</span
+                            >
+                            {filteredRecipes.length === 1
+                                ? "recipe"
+                                : "recipes"}
+                        {/if}
+                    </p>
+                    <div class="hide-scrollbar overflow-y-scroll h-[300px]">
+                        {#each filteredRecipes as { name, slug, ingredients }}
+                            <a
+                                href="/recipes/{slug}"
+                                class="flex w-full items-center justify-between rounded-md bg-transparent p-3 transition hover:bg-white/5"
+                            >
+                                <div>
+                                    <h3 class="text-start">
+                                        {name}: <small>{ingredients}</small>
+                                    </h3>
+                                </div>
+                            </a>
+                        {/each}
+                    </div>
+                </div>
+            {/if}
         </div>
     </div>
 {/if}
@@ -72,9 +80,9 @@
         on:click={() => {
             isSearchVisible = true;
         }}
-        class="flex w-full md:w-96 items-center gap-2 rounded-md bg-none p-2 text-white/50 outline outline-[1.5px] outline-white/10 transition hover:bg-white/10"
+        class="flex items-center justify-center gap-2 rounded-full bg-none p-2 text-white/50 outline outline-[1.5px] outline-white/10 transition hover:bg-white/10"
     >
-        <MagnifyingGlass cls="w-4 h-4" /> Search from {data.recipes.length} recipes
+        <MagnifyingGlass cls="w-4 h-4" />
     </button>
     <div class="grid lg:grid-cols-2 gap-2">
         {#each data.recipes as { name, slug, ingredients, color }}
